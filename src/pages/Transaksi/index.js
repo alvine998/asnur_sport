4c6c1d8b938e488d8d92af8895e6aa50a3007094
alvine <alvinecom2018@gmail.com>
2021-12-21 19:17:00 +0700

    transaksi new

--- src/pages/Transaksi/index.js
+++ src/pages/Transaksi/index.js
@@ -36,45 +36,66 @@ class Transaksi extends Component {
 
                 <a
                   className="btn btn-outline-primary width-cari positioning"
-                //   style={{marginLeft:10}}
+                  //   style={{marginLeft:10}}
                   href="#"
                 >
                   Tambahkan
                 </a>
               </form>
-
-              {/* Table Data Barang Transaksi */}
-              <div className="pad-table">
-                <table class="table table-striped">
-                  <thead>
-                    <tr>
-                      <th scope="col">No</th>
-                      <th scope="col">Nama Barang</th>
-                      <th scope="col">Harga</th>
-                      <th scope="col">Tools</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>@mdo</td>
-                      <td><a className="btn btn-danger">Hapus</a></td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Jacob</td>
-                      <td>@fat</td>
-                      <td><a className="btn btn-danger">Hapus</a></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
             </div>
             <div className="col">
               <div className="">
-                <div className="box-harga"></div>
+                <div className="box-harga">
+                  <h5>Rp. </h5>
+                </div>
               </div>
+              {/* <div className="box-struk">
+                <table className="table table-stripped">
+                  <tr>
+                    <td>No</td>
+                    <td>Nama Barang</td>
+                    <td>Harga</td>
+                    <td>Total Harga</td>
+                  </tr>
+                  <tr>
+                    <td>Hello</td>
+                  </tr>
+                </table>
+              </div> */}
+            </div>
+          </div>
+
+          <div>
+            {/* Table Data Barang Transaksi */}
+            <div className="pad-table">
+              <div style={{paddingBottom:20}}>
+                <a className="btn btn-primary btn-right" href="#">Lanjutkan</a>
+              </div>
+
+              <table class="table table-stripped">
+                <thead>
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama Barang</th>
+                    <th scope="col">Harga</th>
+                    <th scope="col">Tools</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>@mdo</td>
+                    <td><a className="btn btn-danger">Hapus</a></td>
+                  </tr>
+                  <tr>
+                    <th>Total Harga :</th>
+                    <td></td>
+                    <td></td>
+                    <td>Rp. </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
